1fc48b8d61415572cc30d7e5db74c728ac9038e0
admin-gl <gbolou@enssat.fr>
2020-10-26 15:44:30 +0100

    testing + details
    enhanced Money class
    created test directory and MoneyTest class to implement testing with Junit 4.12
    (mvn package to run tests and build)

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,11 @@
+package demo;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        Money amount = new Money(52, "EUR");
+        System.out.println(amount.toString());
+    }
+
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,5 +1,46 @@
 package demo;
 
+
+import java.util.Objects;
+
 public class Money {
 
+    public static class Currency{
+        private String name;
+
+        public Currency(String name) {
+            this.name = name;
+        }
+    }
+
+    public int amount;
+    public String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency=currency;
+    }
+
+    @Override
+    public String toString() {
+        return amount + currency;
+    }
+
+    public Money add(Money that) {
+        return new Money(this.amount+that.amount,this.currency);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money)) return false;
+        Money money = (Money) o;
+        return amount == money.amount &&
+                Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
 }
